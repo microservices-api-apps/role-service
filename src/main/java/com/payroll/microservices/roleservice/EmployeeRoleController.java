e4b2a61e851571057749588e383869001375f34c
Anand <arockiaanandraj@gmail.com>
2019-11-19 22:09:29 +0530

    Added Controller and JPA

---
+++ src/main/java/com/payroll/microservices/roleservice/EmployeeRoleController.java
@@ -0,0 +1,18 @@
+package com.payroll.microservices.roleservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeRoleController {
+
+	@Autowired
+	EmployeeRoleRepository employeeRoleRepository;
+
+	@GetMapping("/role/{roleName}")
+	public EmployeeRole getRoleByRoleName(@PathVariable String roleName) {
+		return employeeRoleRepository.findByRoleName(roleName);
+	}
+}
